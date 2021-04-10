2409d1bf92b23635345b79c1795c1b8d74fa2d6d
vert <vert@vert.com>
2021-04-10 17:20:44 -0400

    update config

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1130,7 +1130,7 @@
  *     But: `M851 Z+1` with a CLEARANCE of 2  =>  2mm from bed to nozzle.
  */
 #define Z_CLEARANCE_DEPLOY_PROBE    5 // Z Clearance for Deploy/Stow
-#define Z_CLEARANCE_BETWEEN_PROBES  3 // Z Clearance between probe points
+#define Z_CLEARANCE_BETWEEN_PROBES  5 // Z Clearance between probe points
 //#define Z_CLEARANCE_MULTI_PROBE     2 // Z Clearance between multiple probes
 //#define Z_AFTER_PROBING             3 // Z position after probing is done
 

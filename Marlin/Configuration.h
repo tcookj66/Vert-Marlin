419a2abc077afb88b42044aea7ee530579322093
vert <vert@vert.com>
2021-04-27 06:47:59 -0400

    clean up

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1230,8 +1230,8 @@
 // @section machine
 
 // The size of the printable area
-#define X_BED_SIZE 200
-#define Y_BED_SIZE 200
+#define X_BED_SIZE 230
+#define Y_BED_SIZE 230
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS -10

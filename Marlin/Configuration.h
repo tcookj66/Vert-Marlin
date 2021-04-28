35a4913e960afb06f7f12666c80a51ec6b5bdaf3
vert <vert@vert.com>
2021-04-28 17:57:20 -0400

    follow up

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1065,7 +1065,7 @@
 #define XY_PROBE_FEEDRATE (40*60)
 
 // Feedrate (mm/min) for the first approach when double-probing (MULTIPLE_PROBING == 2)
-#define Z_PROBE_FEEDRATE_FAST (10*60)
+#define Z_PROBE_FEEDRATE_FAST (5*60)
 
 // Feedrate (mm/min) for the "accurate" probe of each point
 #define Z_PROBE_FEEDRATE_SLOW (Z_PROBE_FEEDRATE_FAST / 2)

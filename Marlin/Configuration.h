e208b2de4099fb25d99a49a97f83c41af9fb71f2
vert <vert@vert.com>
2021-05-10 07:36:42 -0400

    follow up

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -299,7 +299,7 @@
 #define MIXING_EXTRUDER
 #define EXTRUDERS 1
 #if ENABLED(MIXING_EXTRUDER)
-  #define MIXING_STEPPERS 3        // Number of steppers in your mixing extruder
+  #define MIXING_STEPPERS 2        // Number of steppers in your mixing extruder
   #define MIXING_VIRTUAL_TOOLS 16  // Use the Virtual Tool method with M163 and M164
   #define DIRECT_MIXING_IN_G1    // Allow ABCDHI mix factors in G1 movement commands
   #define GRADIENT_MIX           // Support for gradient mixing with M166 and LCD

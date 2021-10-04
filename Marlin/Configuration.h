d64c4a81b9c9d1a973ec7f4fbe06b2ddc17b4c93
Vert <Vert@Vert.com>
2021-10-04 14:45:20 -0400

    update

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -606,9 +606,9 @@
     #define DEFAULT_Ki_LIST {   1.08,   1.08 }
     #define DEFAULT_Kd_LIST { 114.00, 114.00 }
   #else
-    #define DEFAULT_Kp  22.20
-    #define DEFAULT_Ki   1.08
-    #define DEFAULT_Kd 114.00
+    #define DEFAULT_Kp  30.90
+    #define DEFAULT_Ki   3.45
+    #define DEFAULT_Kd  69.20
   #endif
 #endif // PIDTEMP
 
@@ -629,7 +629,7 @@
  * heater. If your configuration is significantly different than this and you don't understand
  * the issues involved, don't use bed PID until someone else verifies that your hardware works.
  */
-//#define PIDTEMPBED
+#define PIDTEMPBED
 
 //#define BED_LIMIT_SWITCHING
 
